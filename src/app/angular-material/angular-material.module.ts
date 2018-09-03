8296cf133d4883c5f7309bb7a579ac3ee7acf326
shudong <zhangshudong@gmail.com>
2018-09-03 23:34:39 +0800

    Add shared module

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FancyCardComponent } from './fancy-card/fancy-card.component';
-import {MatCardModule} from '@angular/material';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FancyCardComponent} from './fancy-card/fancy-card.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatCardModule,
+    SharedModule
   ],
   exports: [
     FancyCardComponent
   ],
   declarations: [FancyCardComponent]
 })
-export class AngularMaterialModule { }
+export class AngularMaterialModule {
+}
